122bfa63ee9346127bee5eccd98073a0d6edadc3
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2021-02-15 15:29:01 +0900

    feat: 🎸 remake 1 hash-table

--- src/1.two-sum.ts
+++ src/1.two-sum.ts
@@ -5,22 +5,21 @@
  */
 
 // @lc code=start
+// time O{n}
+// space O{n}
 function twoSum(nums: number[], target: number): number[] {
-  // sortできない もとのindex必要
-  // 探す
-  for(let i=0;i<nums.length;i++){
-    for (let j=i+1;j<nums.length;j++){
-      if(i===j){
-        continue
+  let map = {};
+  
+  for (let i = 0; i < nums.length; i++) {
+      const n = map[target - nums[i]];
+      if (n !== undefined && n !== i) {
+          return [i, map[target - nums[i]]];
       }
-      if(nums[i]+nums[j]===target){
-        return [i,j]
-      }
-    }
-
+      // setTable {element:index}
+      map[nums[i]] = i;
   }
-  return  [0,0]
 
-};
+  return [];
+}
 // @lc code=end
 
