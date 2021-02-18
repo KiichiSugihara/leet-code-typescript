9cd175f307621abc0cb4af2f4198e1b21688493e
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2021-02-18 13:41:36 +0900

    feat: 🎸 finish 141

---
+++ src/141.linked-list-cycle.ts
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=141 lang=typescript
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function hasCycle(head: ListNode | null): boolean {
+  if (head === null || head.next === null) return false;
+  let fast = head,
+    slow = head;
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next.next;
+    slow = slow.next;
+    if (fast === slow) {
+      return true;
+    }
+  }
+
+  return false;
+}
+// @lc code=end
