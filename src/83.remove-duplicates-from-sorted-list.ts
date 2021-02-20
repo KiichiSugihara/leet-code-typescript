74fa79acdb4768cbb180d79bfbf6851fa6fa5bf2
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2021-02-20 12:38:42 +0900

    feat: 🎸 setup 83

---
+++ src/83.remove-duplicates-from-sorted-list.ts
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=83 lang=typescript
+ *
+ * [83] Remove Duplicates from Sorted List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  return head;
+}
+// @lc code=end
