c9d1e22cb4f805fb4a60655ca7031b5a7c457cce
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2021-02-27 16:18:16 +0900

    feat: 🎸 finish 703 pre

--- src/703.kth-largest-element-in-a-stream.ts
+++ src/703.kth-largest-element-in-a-stream.ts
@@ -5,10 +5,32 @@
  */
 
 // @lc code=start
+const compare = (a: number, b: number) => b - a;
 class KthLargest {
-  constructor(k: number, nums: number[]) {}
-
-  add(val: number): number {}
+  kthNumber: number;
+  nums: number[];
+  constructor(kthNumber: number, nums: number[]) {
+    this.kthNumber = kthNumber;
+    this.nums = nums;
+    // quick-sort
+    this.nums.sort(compare);
+  }
+  add(val: number): number {
+    //  add number to sorted numbers
+    const insert = () => {
+      if (
+        this.nums[this.nums.length - 1] < val ||
+        this.nums.length < this.kthNumber
+      ) {
+        this.nums.push(val);
+        this.nums = this.nums.sort(compare);
+        this.nums.length =
+          this.nums.length > this.kthNumber ? this.kthNumber : this.nums.length;
+      }
+    };
+    insert();
+    return this.nums[this.nums.length - 1];
+  }
 }
 
 /**
