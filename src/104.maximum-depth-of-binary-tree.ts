692961f45223642a8adfdeed46ad3bb7bb500e77
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2021-04-05 09:12:00 +0900

    feat: 🎸 make 104

---
+++ src/104.maximum-depth-of-binary-tree.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=104 lang=typescript
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+
+};
+// @lc code=end
+
